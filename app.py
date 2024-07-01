bc879e9cadc4027900e31a6ab3012fa77fd2064a
Garry <145930725+Garry864@users.noreply.github.com>
2024-07-01 15:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,7 @@ gcp_service_account = st.secrets["gcp_service_account"]
 database_secrets = st.secrets["database"]
 
 # Set Google Cloud credentials environment variables
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json.dumps({
-    "type": gcp_service_account["type"],
-    "project_id": gcp_service_account["project_id"],
-    "private_key_id": gcp_service_account["private_key_id"],
-    "private_key": gcp_service_account["private_key"],
-    "client_email": gcp_service_account["client_email"],
-    "client_id": gcp_service_account["client_id"],
-    "auth_uri": gcp_service_account["auth_uri"],
-    "token_uri": gcp_service_account["token_uri"],
-    "auth_provider_x509_cert_url": gcp_service_account["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": gcp_service_account["client_x509_cert_url"]
-})
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets["gcp_service_account"]["GOOGLE_APPLICATION_CREDENTIALS"]
 
 
 
